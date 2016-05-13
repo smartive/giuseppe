25059abc57ab8ac691a66ca5fef6473a79dd2fa1
Christoph Bühler <christoph@smartive.ch>
2016-05-13 08:55:57 +0200

    fix docs, add link to middleware.md file

--- routes/RouteDecorators.ts
+++ routes/RouteDecorators.ts
@@ -35,7 +35,7 @@ export class RouteRegistration {
  *
  * @param {string} [route=''] - The routed endpoint of the method. If omitted, the base route is taken.
  * @param {RouteMethod} httpMethod - The http verb to use for this route.
- * @param {RequestHandler[]} middlewares - The used middleware(s) for this route.
+ * @param {RequestHandler[]} [middlewares=[]] - The used middleware(s) for this route.
  * @returns {(any, string, PropertyDescriptor) => void} - Method decorator for the given function.
  */
 export function Route(route: string = '', httpMethod: RouteMethod = RouteMethod.Get, ...middlewares: RequestHandler[]) {
@@ -50,7 +50,7 @@ export function Route(route: string = '', httpMethod: RouteMethod = RouteMethod.
  * Alias function for @Route(string, RouteMethod.Get)
  *
  * @param {string} [route=''] - The routed endpoint of the method. If omitted, the base route is taken.
- * @param {RequestHandler[]} middlewares - The used middleware(s) for this route.
+ * @param {RequestHandler[]} [middlewares=[]] - The used middleware(s) for this route.
  * @returns {(any, string, PropertyDescriptor) => void} - Method decorator for the given function with the http verb 'GET'.
  */
 export function Get(route: string = '', ...middlewares: RequestHandler[]) {
@@ -61,7 +61,7 @@ export function Get(route: string = '', ...middlewares: RequestHandler[]) {
  * Alias function for @Route(string, RouteMethod.Put)
  *
  * @param {string} [route=''] - The routed endpoint of the method. If omitted, the base route is taken.
- * @param {RequestHandler[]} middlewares - The used middleware(s) for this route.
+ * @param {RequestHandler[]} [middlewares=[]] - The used middleware(s) for this route.
  * @returns {(any, string, PropertyDescriptor) => void} - Method decorator for the given function with the http verb 'PUT'.
  */
 export function Put(route: string = '', ...middlewares: RequestHandler[]) {
@@ -72,7 +72,7 @@ export function Put(route: string = '', ...middlewares: RequestHandler[]) {
  * Alias function for @Route(string, RouteMethod.Post)
  *
  * @param {string} [route=''] - The routed endpoint of the method. If omitted, the base route is taken.
- * @param {RequestHandler[]} middlewares - The used middleware(s) for this route.
+ * @param {RequestHandler[]} [middlewares=[]] - The used middleware(s) for this route.
  * @returns {(any, string, PropertyDescriptor) => void} - Method decorator for the given function with the http verb 'POST'.
  */
 export function Post(route: string = '', ...middlewares: RequestHandler[]) {
@@ -83,7 +83,7 @@ export function Post(route: string = '', ...middlewares: RequestHandler[]) {
  * Alias function for @Route(string, RouteMethod.Delete)
  *
  * @param {string} [route=''] - The routed endpoint of the method. If omitted, the base route is taken.
- * @param {RequestHandler[]} middlewares - The used middleware(s) for this route.
+ * @param {RequestHandler[]} [middlewares=[]] - The used middleware(s) for this route.
  * @returns {(any, string, PropertyDescriptor) => void} - Method decorator for the given function with the http verb 'DELETE'.
  */
 export function Delete(route: string = '', ...middlewares: RequestHandler[]) {
@@ -94,7 +94,7 @@ export function Delete(route: string = '', ...middlewares: RequestHandler[]) {
  * Alias function for @Route(string, RouteMethod.Head)
  *
  * @param {string} [route=''] - The routed endpoint of the method. If omitted, the base route is taken.
- * @param {RequestHandler[]} middlewares - The used middleware(s) for this route.
+ * @param {RequestHandler[]} [middlewares=[]] - The used middleware(s) for this route.
  * @returns {(any, string, PropertyDescriptor) => void} - Method decorator for the given function with the http verb 'Head'.
  */
 export function Head(route: string = '', ...middlewares: RequestHandler[]) {
