725db4fcb1c4edbd2851dc8439418ec91d755b36
Christoph Bühler <christoph@smartive.ch>
2016-07-13 14:26:53 +0200

    readd docs

--- index.ts
+++ index.ts
@@ -16,5 +16,26 @@ const paramHandler = new ParamHandler();
 const routeHandler = new RouteHandler(paramHandler);
 const registrar = new Registrar(routeHandler, paramHandler);
 
+/**
+ * Function that loads and registers all controllers from a given directory. All found files are "required" and
+ * should not throw any errors, or else the promise is rejected.
+ *
+ * @param {string} folderPath - The root path to start the search for the controllers.
+ * @param {string} [root=process.cwd()] - The root of the project. Can be different if you start your app through a script.
+ * @param {boolean} [recursive=false] - Defines if the function searches recursively for controllers.
+ * @param {RegExp} [matchRegExp=/(.*)[.]js$/] - The regular expression that must be mached before a file is required.
+ * @param {string} [baseUrl=''] - Base url for the routing system. Will be prefixed for all controllers.
+ * @param {Router} [router=Router()] - Express router to attach the routes to. If omitted, a new router is instantiated.
+ * @returns {Promise<Router>} - A promise that resolves with the configured router instance. Or rejects when an error happens.
+ */
 export const registerControllersFromFolder = registrar.registerControllersFromFolder;
+
+/**
+ * Function that registers all decorated controller with their decorated route functions in an expressJS router.
+ * When no router is provided, this function will instantiate and return a router.
+ *
+ * @param {string} [baseUrl=''] - Base url for the routing system. Will be prefixed for all controllers.
+ * @param {Router} [router=Router()] - Express router to attach the routes to. If omitted, a new router is instantiated.
+ * @returns {Router} - The configured router.
+ */
 export const registerControllers = registrar.registerControllers;
