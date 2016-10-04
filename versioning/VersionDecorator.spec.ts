cc19cf01b8a5146a3905faba7db61f44ccad30e4
Christoph Bühler <christoph@smartive.ch>
2016-10-04 11:05:07 +0200

    add files for version decorator, add decorate test stubs

---
+++ versioning/VersionDecorator.spec.ts
@@ -0,0 +1,24 @@
+import 'reflect-metadata';
+import chai = require('chai');
+
+chai.should();
+
+describe('VersionDecorator', () => {
+
+    it('should add a version information to a controller');
+
+    it('should add a version information to a route');
+
+    it('should add a correct from version');
+
+    it('should add a correct until version');
+
+    it('should add a correct from / until version');
+
+    it('should throw if from is not a normal number');
+
+    it('should throw if until is not a normal number');
+
+    it('should throw if neither fron nor until is declared');
+
+});
