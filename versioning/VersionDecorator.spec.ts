562ecf63d172017a84a3d00bfbe6e5dc9395457f
Christoph Bühler <christoph@smartive.ch>
2016-10-04 15:56:45 +0200

    add first error cases

--- versioning/VersionDecorator.spec.ts
+++ versioning/VersionDecorator.spec.ts
@@ -1,9 +1,18 @@
 import 'reflect-metadata';
+import { IocContainer } from '../core/IoC';
+import { IoCSymbols } from '../core/IoCSymbols';
+import { Registrar } from '../core/Registrar';
+import { VersionInformationInvalid, VersionInformationMissing } from '../errors/Errors';
+import { Version } from './VersionDecorator';
 import chai = require('chai');
 
 chai.should();
 
 describe('VersionDecorator', () => {
+
+    afterEach(() => {
+        IocContainer.get<Registrar>(IoCSymbols.registrar).resetControllerRegistrations();
+    });
 
     it('should add a version information to a controller');
 
@@ -15,11 +24,60 @@ describe('VersionDecorator', () => {
 
     it('should add a correct from / until version');
 
-    it('should throw if from is not a normal number');
+    it('should throw if from is not a normal number', () => {
+        (() => {
+            @Version({ from: 3.14 })
+            class Ctrl {
 
-    it('should throw if until is not a normal number');
+            }
+        }).should.throw(VersionInformationInvalid);
 
-    it('should throw if neither from nor until is declared');
+        (() => {
+            @Version({ from: <any>"1" })
+            class Ctrl {
+
+            }
+        }).should.throw(VersionInformationInvalid);
+
+        (() => {
+            @Version({ from: 0 })
+            class Ctrl {
+
+            }
+        }).should.throw(VersionInformationInvalid);
+    });
+
+    it('should throw if until is not a normal number', () => {
+        (() => {
+            @Version({ until: 3.14 })
+            class Ctrl {
+
+            }
+        }).should.throw(VersionInformationInvalid);
+
+        (() => {
+            @Version({ until: <any>"1" })
+            class Ctrl {
+
+            }
+        }).should.throw(VersionInformationInvalid);
+
+        (() => {
+            @Version({ until: 0 })
+            class Ctrl {
+
+            }
+        }).should.throw(VersionInformationInvalid);
+    });
+
+    it('should throw if neither from nor until is declared', () => {
+        (() => {
+            @Version({})
+            class Ctrl {
+
+            }
+        }).should.throw(VersionInformationMissing);
+    });
 
     it('should throw if from is greater then until');
 
---
+++ versioning/VersionDecorator.ts
@@ -0,0 +1,32 @@
+import { VersionInformationInvalid, VersionInformationMissing } from '../errors/Errors';
+
+function isDefined(value: any): boolean {
+    return value !== null && value !== undefined;
+}
+
+/**
+ * Version decorator; decorates a class or a method of a class as versioned.
+ * When the routes are registered, the version information will determine
+ * if a route is accessable or not. If a route is not available in a certain version
+ * the system should return a 404.
+ *
+ * @param {{from: ?number, to: ?number}} versionInformation - An object with from and / or until information. If both are omitted, an error is thrown.
+ * @returns {(Function) => void} - Decorator for a versioned controller or route.
+ */
+export function Version(versionInformation: { from?: number, until?: number }) {
+    return (controllerOrRoute: any) => {
+        if (!(isDefined(versionInformation.from) || isDefined(versionInformation.until))) {
+            throw new VersionInformationMissing(controllerOrRoute.name);
+        }
+
+        if (isDefined(versionInformation.from) && (versionInformation.from.constructor !== Number || versionInformation.from < 1 || versionInformation.from % 1 !== 0)) {
+            throw new VersionInformationInvalid(controllerOrRoute.name, `The from value (${versionInformation.from}) is either not a number, a floating point number or less than 1`);
+        }
+
+        if (isDefined(versionInformation.until) && (versionInformation.until.constructor !== Number || versionInformation.until < 1 || versionInformation.until % 1 !== 0)) {
+            throw new VersionInformationInvalid(controllerOrRoute.name, `The until value (${versionInformation.until}) is either not a number, a floating point number or less than 1`);
+        }
+
+        console.log(controllerOrRoute);
+    };
+}
