b94a04c2fb7e58c9503013b1301f68c12e3f7508
Christoph Bühler <christoph@smartive.ch>
2016-10-06 11:51:05 +0200

    missed inversify

--- core/InitialConfiguration.ts
+++ core/InitialConfiguration.ts
@@ -1,5 +1,7 @@
 import { Configuration } from './Configuration';
+import { injectable } from 'inversify';
 
+@injectable()
 export class InitialConfiguration implements Configuration {
     public versionHeaderName = 'Accept-Version';
 }
