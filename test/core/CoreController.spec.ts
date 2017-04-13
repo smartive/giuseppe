1286718d8dbef713d7e4b06fd85745508fec25ff
Christoph Bühler <christoph@smartive.ch>
2017-04-13 11:17:45 +0200

    adding first ctrl to giusi

---
+++ test/core/CoreController.spec.ts
@@ -0,0 +1,29 @@
+import 'reflect-metadata';
+import { Giuseppe } from '../../src/';
+import { Controller } from '../../src/core/controller/GiuseppeController';
+import chai = require('chai');
+import sinonChai = require('sinon-chai');
+
+chai.should();
+chai.use(sinonChai);
+
+describe('Core controller', () => {
+
+    describe('@Decorator', () => {
+
+        let giuseppe: Giuseppe;
+
+        beforeEach(() => {
+            giuseppe = new Giuseppe();
+        });
+
+        it('should register a controller in giuseppe.', () => {
+            @Controller()
+            class Ctrl { }
+
+            giuseppe.controller.should.have.length(1);
+        });
+
+    });
+
+});
