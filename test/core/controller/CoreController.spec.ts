fae9fce85ebd2a67f253ba78cbf32d72a2a7304f
Christoph Bühler <christoph@smartive.ch>
2017-05-09 10:15:23 +0200

    adding test files

---
+++ test/core/controller/CoreController.spec.ts
@@ -0,0 +1,148 @@
+import 'reflect-metadata';
+import { Giuseppe } from '../../../src/';
+import { Controller } from '../../../src/core/controller/GiuseppeApiController';
+import { Get } from '../../../src/core/routes';
+import chai = require('chai');
+import sinonChai = require('sinon-chai');
+
+const should = chai.should();
+chai.use(sinonChai);
+
+describe('Core controller', () => {
+
+    describe('@Controller decorator', () => {
+
+        let giuseppe: Giuseppe;
+
+        beforeEach(() => {
+            giuseppe = new Giuseppe();
+        });
+
+        afterEach(() => {
+            (Giuseppe.registrar as any).controller = [];
+        });
+
+        it('should return Controller decorator', () => {
+            Controller().should.be.a('function');
+        });
+
+        it('should register a controller in giuseppe.', () => {
+            @Controller()
+            class Ctrl { }
+
+            Giuseppe.registrar.controller.should.have.length(1);
+        });
+
+        it('should register the correct contextual target', () => {
+            @Controller()
+            class Ctrl { }
+
+            Giuseppe.registrar.controller[0].ctrlTarget.should.equal(Ctrl);
+        });
+
+        it('should use the correct route prefix', () => {
+            @Controller('foobar')
+            class Ctrl { }
+
+            (Giuseppe.registrar.controller[0] as any).routePrefix.should.equal('foobar');
+        });
+
+        it('should use the correct middlewares when prefix is set', () => {
+            const fn = (req, res, next) => { next(); };
+            @Controller('foobar', fn)
+            class Ctrl { }
+
+            (Giuseppe.registrar.controller[0] as any).routePrefix.should.equal('foobar');
+            Giuseppe.registrar.controller[0].middlewares[0].should.equal(fn);
+        });
+
+        it('should use the correct middlewares when prefix is not set', () => {
+            const fn = (req, res, next) => { next(); };
+            @Controller(fn)
+            class Ctrl { }
+
+            (Giuseppe.registrar.controller[0] as any).routePrefix.should.equal('');
+            Giuseppe.registrar.controller[0].middlewares[0].should.equal(fn);
+        });
+
+        it('should use the correct middlewares for multiple functions with a prefix', () => {
+            const fn = (req, res, next) => { next(); },
+                fn2 = (req, res, next) => { next(); };
+            @Controller('foobar', fn, fn2)
+            class Ctrl { }
+
+            (Giuseppe.registrar.controller[0] as any).routePrefix.should.equal('foobar');
+            Giuseppe.registrar.controller[0].middlewares[0].should.equal(fn);
+            Giuseppe.registrar.controller[0].middlewares[1].should.equal(fn2);
+        });
+
+        it('should use the correct middlewares for multiple functions', () => {
+            const fn = (req, res, next) => { next(); },
+                fn2 = (req, res, next) => { next(); };
+            @Controller(fn, fn2)
+            class Ctrl { }
+
+            (Giuseppe.registrar.controller[0] as any).routePrefix.should.equal('');
+            Giuseppe.registrar.controller[0].middlewares[0].should.equal(fn);
+            Giuseppe.registrar.controller[0].middlewares[1].should.equal(fn2);
+        });
+
+        it('should throw if the controller (plugin) is not registered', done => {
+            (giuseppe as any).plugins = [];
+
+            @Controller()
+            class Ctrl { }
+
+            try {
+                giuseppe.start();
+                done(new Error('did not throw.'));
+            } catch (e) {
+                done();
+            }
+        });
+
+        it(`should return it's registered routes`, () => {
+            @Controller()
+            class Ctrl {
+                @Get()
+                public get(): void { }
+            }
+
+            const ctrl = Giuseppe.registrar.controller[0];
+            const route = ctrl.createRoutes('')[0];
+            route.id.should.equal('get_');
+            route.name.should.equal('get');
+        });
+
+        it('should return the correct route urls for default', () => {
+            @Controller('api')
+            class Ctrl {
+                @Get('foobar')
+                public get(): void { }
+
+                @Get('/barfoo')
+                public get2(): void { }
+                
+                @Get('~/rootfoo')
+                public get3(): void { }
+            }
+
+            const ctrl = Giuseppe.registrar.controller[0],
+                routes = ctrl.createRoutes('');
+            
+            let route = routes[0];
+            route.id.should.equal('get_foobar');
+            route.url.should.equal('api/foobar');
+
+            route = routes[1];
+            route.id.should.equal('get_/barfoo');
+            route.url.should.equal('api/barfoo');
+            
+            route = routes[2];
+            route.id.should.equal('get_~/rootfoo');
+            route.url.should.equal('rootfoo');
+        });
+
+    });
+
+});
---
+++ test/errors/ErrorHandlerDecorator.spec.ts
@@ -0,0 +1,198 @@
+import 'reflect-metadata';
+import { Giuseppe } from '../../src';
+import { Controller } from '../../src/core/controller/GiuseppeApiController';
+import { Get } from '../../src/core/routes';
+import {
+    ErrorHandlerWrongArgumentsError,
+    ErrorHandlerWrongArgumentTypesError,
+    ErrorHandlerWrongReturnTypeError,
+} from '../../src/errors';
+import { ErrorHandler } from '../../src/errors/ErrorHandlerDecorator';
+import { ControllerMetadata } from '../../src/utilities/ControllerMetadata';
+import chai = require('chai');
+import * as sinon from 'sinon';
+import sinonChai = require('sinon-chai');
+
+const should = chai.should();
+chai.use(sinonChai);
+
+class TestRouter {
+    public routes: { [id: string]: Function } = {};
+
+    public get(route: string, func: Function): void {
+        this.routes[route] = func;
+    }
+
+    public put(route: string, func: Function): void {
+        this.routes[route] = func;
+    }
+
+    public post(route: string, func: Function): void {
+        this.routes[route] = func;
+    }
+
+    public delete(route: string, func: Function): void {
+        this.routes[route] = func;
+    }
+
+    public head(route: string, func: Function): void {
+        this.routes[route] = func;
+    }
+}
+
+describe('ErrorHandlerDecorators', () => {
+
+    afterEach(() => {
+        (Giuseppe.registrar as any).controller = [];
+    });
+
+    it('should throw on wrong handler argument count', () => {
+        const fn = () => {
+            class Ctrl {
+                @ErrorHandler()
+                public func(): void {
+                }
+            }
+        };
+
+        fn.should.throw(ErrorHandlerWrongArgumentsError);
+    });
+
+    it('should throw on wrong handler argument types', () => {
+        const fn = () => {
+            class Ctrl {
+                @ErrorHandler()
+                public func(req: any, res: string, err: number): void {
+                }
+            }
+        };
+
+        fn.should.throw(ErrorHandlerWrongArgumentTypesError);
+    });
+
+    it('should throw on wrong handler return type', () => {
+        const fn = () => {
+            class Ctrl {
+                @ErrorHandler()
+                public func(req: Object, res: Object, err: Error): number {
+                    return 0;
+                }
+            }
+        };
+
+        fn.should.throw(ErrorHandlerWrongReturnTypeError);
+    });
+
+    it('should register a handler for the controller', () => {
+        class Ctrl {
+            @ErrorHandler()
+            public func(req: Object, res: Object, err: Error): void {
+            }
+        }
+
+        const errorManager: any = new ControllerMetadata(Ctrl.prototype).errorHandler();
+
+        errorManager.handlers.Error
+            .should.be.a('function')
+            .with.lengthOf(3)
+            .that.equals(Ctrl.prototype.func);
+    });
+
+    it('should register a default and a specific handler for the controller', () => {
+        class Ctrl {
+            @ErrorHandler()
+            public func(req: Object, res: Object, err: Error): void {
+            }
+
+            @ErrorHandler(ErrorHandlerWrongReturnTypeError, ErrorHandlerWrongArgumentsError)
+            public func2(req: Object, res: Object, err: Error): void {
+            }
+        }
+
+        const errorManager: any = new ControllerMetadata(Ctrl.prototype).errorHandler();
+
+        errorManager.handlers.Error
+            .should.be.a('function')
+            .with.lengthOf(3)
+            .that.deep.equals(Ctrl.prototype.func);
+
+        errorManager.handlers.ErrorHandlerWrongReturnTypeError
+            .should.be.a('function')
+            .with.lengthOf(3)
+            .that.deep.equals(Ctrl.prototype.func2);
+
+        errorManager.handlers.ErrorHandlerWrongArgumentsError
+            .should.be.a('function')
+            .with.lengthOf(3)
+            .that.deep.equals(Ctrl.prototype.func2);
+    });
+
+    it('should set the correct this context', done => {
+        @Controller()
+        class Ctrl {
+            private test: string = 'foobar';
+
+            @Get()
+            public func() {
+                throw 'Foobar.';
+            }
+
+            @ErrorHandler()
+            public error(req: Object, res: Object, err: Error): void {
+                try {
+                    this.test.should.equal('foobar');
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            }
+        }
+
+        const router = new TestRouter(),
+            giuseppe = new Giuseppe();
+
+        giuseppe.router = router as any;
+        giuseppe.start();
+
+        router.routes['/'].apply(null, [{}, {}]);
+    });
+
+    it('should call the most specific error handler', () => {
+        class FoobarError extends Error { }
+
+        @Controller()
+        class Ctrl {
+            @Get()
+            public getErr(): string {
+                throw new FoobarError();
+            }
+
+            @ErrorHandler()
+            public func(req: Object, res: Object, err: Error): void {
+            }
+
+            @ErrorHandler(FoobarError)
+            public func2(req: Object, res: Object, err: Error): void {
+            }
+        }
+
+        const router = new TestRouter(),
+            giuseppe = new Giuseppe(),
+            errorManager: any = new ControllerMetadata(Ctrl.prototype).errorHandler();
+
+        giuseppe.router = router as any;
+        giuseppe.start();
+
+        const spy = sinon.spy(),
+            spy2 = sinon.spy();
+
+        errorManager.addHandler(spy, Error);
+        errorManager.addHandler(spy2, FoobarError);
+
+        router.routes['/'].apply(this, [{}, {}]);
+
+        spy.should.not.be.called;
+        spy2.should.be.calledOnce;
+    });
+
+});
