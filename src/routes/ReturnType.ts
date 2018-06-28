e48372593bb2f2ff9d9cf58fc9ab0f02364069bf
Christoph Bühler <christoph@smartive.ch>
2018-06-28 08:47:08 +0200

    feat: let returntype define it's value type

--- src/routes/ReturnType.ts
+++ src/routes/ReturnType.ts
@@ -2,16 +2,17 @@
  * Return type handler. This element registers a hander for specific return types. When an object is received from
  * a route function, it's constructor name is checked against all various registered return type handler. When no one
  * is found, giuseppe throws an error, otherwise the registered handler (or the default) will be used.
- * 
+ *
  * @export
  * @interface ReturnType
- * @template T 
+ * @template TValue Type for the incomming value
+ * @template TResult Type for the returned value of the `getValue` function
  */
-export interface ReturnType<T> {
+export interface ReturnType<TValue, TResult = string> {
     /**
      * The name of the type that is handled. This can be 'String', 'Number', or anything else.
      * One special name is possible: 'default'. The default is used whenever no other types that matches are found.
-     * 
+     *
      * @type {string}
      * @memberof ReturnType
      */
@@ -19,29 +20,29 @@ export interface ReturnType<T> {
 
     /**
      * Returns the headers for the given value. The function must return something. Even if it's an empty header hash.
-     * 
-     * @param {T} [value] 
-     * @returns {{ [field: string]: string }} 
+     *
+     * @param {TValue} [value]
+     * @returns {{ [field: string]: string }}
      * @memberof ReturnType
      */
-    getHeaders(value?: T): { [field: string]: string };
+    getHeaders(value?: TValue): { [field: string]: string };
 
     /**
      * Get the return http status code for the given value.
-     * 
-     * @param {T} [value] 
-     * @returns {number} 
+     *
+     * @param {TValue} [value]
+     * @returns {number}
      * @memberof ReturnType
      */
-    getStatus(value?: T): number;
+    getStatus(value?: TValue): number;
 
     /**
      * Get the stringified value for a given value. This value is sent by express with the set headers and
      * status codes. This function is only called if an actual value exists.
-     * 
-     * @param {T} value 
-     * @returns {string} 
+     *
+     * @param {TValue} value
+     * @returns {string}
      * @memberof ReturnType
      */
-    getValue(value: T): string;
+    getValue(value: TValue): TResult;
 }
